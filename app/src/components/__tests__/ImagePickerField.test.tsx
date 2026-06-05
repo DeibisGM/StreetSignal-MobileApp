a64f9076c5b9b9a883f3389a070cc2315f58b6ab
DeibisGM <john.gutierrez.morales@est.una.ac.cr>
2026-06-04 21:57:12 -0600

    Wrap test renderer calls with act
    Import act from react-test-renderer and wrap renderer.create(...).toJSON() calls in act for component tests (ErrorMessage, ImagePickerField, ReportCard, StatusBadge). This ensures React updates/effects are flushed and silences act-related warnings in snapshots; no functional changes to the components.

--- app/src/components/__tests__/ImagePickerField.test.tsx
+++ app/src/components/__tests__/ImagePickerField.test.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {act} from 'react-test-renderer';
 import {ImagePickerField} from '../ImagePickerField';
 
 describe('ImagePickerField', () => {
   it('renders empty state', () => {
-    const tree = renderer
-      .create(<ImagePickerField onPick={() => {}} label="Foto" />)
-      .toJSON();
+    let tree;
+    act(() => {
+      tree = renderer
+        .create(<ImagePickerField onPick={() => {}} label="Foto" />)
+        .toJSON();
+    });
     expect(tree).toMatchSnapshot();
   });
 
   it('renders with selected image', () => {
-    const tree = renderer
-      .create(
-        <ImagePickerField
-          value="file:///tmp/photo.jpg"
-          onPick={() => {}}
-          onRemove={() => {}}
-        />,
-      )
-      .toJSON();
+    let tree;
+    act(() => {
+      tree = renderer
+        .create(
+          <ImagePickerField
+            value="file:///tmp/photo.jpg"
+            onPick={() => {}}
+            onRemove={() => {}}
+          />,
+        )
+        .toJSON();
+    });
     expect(tree).toMatchSnapshot();
   });
 
   it('renders disabled state', () => {
-    const tree = renderer
-      .create(<ImagePickerField onPick={() => {}} disabled />)
-      .toJSON();
+    let tree;
+    act(() => {
+      tree = renderer
+        .create(<ImagePickerField onPick={() => {}} disabled />)
+        .toJSON();
+    });
     expect(tree).toMatchSnapshot();
   });
 });
